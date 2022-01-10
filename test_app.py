2a1d0d3786fb113d084309ca10d99f8b534dce68
Dawid Bialek <pelcuk@gmail.com>
2022-01-10 11:41:14 +0100

    Added few very basic tests

---
+++ test_app.py
@@ -0,0 +1,35 @@
+import app as user_repos
+import json
+
+def test_homepage():
+
+    client = user_repos.app.test_client()
+    url = '/'
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+def test_about():
+    client = user_repos.app.test_client()
+    url = '/about'
+    response = client.get(url)
+
+    assert response.status_code == 200
+
+def test_login():
+    client = user_repos.app.test_client()
+    url = '/login'
+    response = client.get(url)
+
+    assert response.status_code == 302
+    assert response.location.startswith('https://github.com/login/oauth/authorize')
+    
+
+def test_authorize_fail():
+    client = user_repos.app.test_client()
+    url = '/authorize'
+    response = client.get(url)
+
+    assert response.status_code == 400 # no token
+
+
